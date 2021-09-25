a309dd489ddd0f320a0b88e2b8b2d47a229a6a86
93virus <bikasadhikari333@gmail.com>
2021-09-26 00:04:52 +0530

    added update user code

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
+const User = require("../models/Users");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req,res) => {
-    res.send("Welcome to users page");
+//update user
+router.put("/:id", async(req,res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch(err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account has been updated..");
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can update only your account!");
+    }
 })
 
 module.exports = router;
