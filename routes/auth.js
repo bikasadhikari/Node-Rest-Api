da599eb33a1b10eb87d0f029a1a84a15912a6247
93virus <bikasadhikari333@gmail.com>
2021-09-25 23:42:11 +0530

    Added Login auth

--- routes/auth.js
+++ routes/auth.js
@@ -26,5 +26,19 @@ router.post("/register", async (req,res) => {
 
 
 //LOGIN
+router.post("/login", async (req,res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).send("User not found!");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).send("Wrong Password!");
+
+        res.status(200).send(user);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 
 module.exports = router;
