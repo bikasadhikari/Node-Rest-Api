43197d2380f5a887c77f1a0140c83bdcf0271eca
93virus <bikasadhikari333@gmail.com>
2021-09-26 22:33:16 +0530

    added update a post code

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,19 @@ router.post("/", async (req, res) => {
     }
 })
 
+//update a post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({$set: req.body});
+            res.status(200).json("The post has been updated...");
+        } else {
+            return res.status(403).json("You can update only your posts!");
+        }
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 module.exports = router;
