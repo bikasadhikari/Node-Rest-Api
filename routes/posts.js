d5f0dcfa0b91d1f618e62585a484ed61f176d5ed
93virus <bikasadhikari333@gmail.com>
2021-09-26 23:30:04 +0530

    added get a post code

--- routes/posts.js
+++ routes/posts.js
@@ -58,14 +58,4 @@ router.put("/:id/like", async (req, res) => {
     }
 });
 
-//get a post 
-router.get("/:id", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        res.status(200).json(post);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-});
-
 module.exports = router;
