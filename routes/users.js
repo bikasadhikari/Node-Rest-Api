e0ad4a593f75aedbe38a649c101cd801a94e4fcc
93virus <bikasadhikari333@gmail.com>
2021-10-04 18:01:31 +0530

    added get friends code

--- routes/users.js
+++ routes/users.js
@@ -41,15 +41,37 @@ router.delete("/:id", async (req, res) => {
 });
 
 //get a user
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
     try {
-        const user = await User.findById(req.params.id);
+        const user = userId ? await User.findById(userId) : await User.findOne({username: username});
         const {password, updatedAt, ...others} = user._doc;
         res.status(200).json(others);
     } catch(err) {
         return res.status(500).json(err);
     }
 });
+
+//get friends
+router.get("/friends/:userId", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const friends = await Promise.all(
+            user.following.map(friendId => {
+                return User.findById(friendId);
+            })
+        );
+        const friendList = [];
+        friends.map(friend => {
+            const {_id,username,profilePicture} = friend;
+            friendList.push({_id,username,profilePicture});
+        });
+        return res.status(200).json(friendList);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 
 //follow a user
 router.put("/:id/follow" , async (req, res) => {
